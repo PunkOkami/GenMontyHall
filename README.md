5fdf29c8c90cf3becfdfae72aa7b1ad4a788ae81
PunkOkami <maks.chmiel98@gmail.com>
2024-04-20 01:39:54 +0200

    Added functions to calculate change of winning and sctipt now runs both scenarios

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # GenMontyHall
 Generalisation of Monty Hall problem to any natural number of doors
 
+Script calculates probability of winning with number of doors chosen when switching doors and when not switching doors as well it then simulates chosen number of tests
+
 ## Used packages:
 - [tqdm](https://github.com/tqdm/tqdm)
 
@@ -11,8 +13,17 @@ python monty_hall_gen.py
 ```
 Output:
 ```shell
-100%|██████████| 100000000/100000000 [02:38<00:00, 632281.07it/s]
-66.66%
+Probability of winning with 3 doors when switching: 66.67%
+
+Simulating games with 3 doors and switching...
+100%|██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 100000000/100000000 [02:10<00:00, 766275.21it/s]
+Percentage of games won: 66.67%
+
+Probability of winning with 3 doors but no switching: 33.33%
+
+Simulating games with 3 doors but no switching...
+100%|██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 100000000/100000000 [01:44<00:00, 958563.34it/s]
+Percentage of games won: 33.33%
 ```
 
 ### Options
@@ -21,7 +32,6 @@ python monty_hall_gen.py -h
 ```
 - -t/--tests - number of tests for each simulation
 - -d/--doors - number of doors
-- -s/--no_switch - flag specifying not to switch doors
 
 ## Note
 All code and math functions used in this repo were created without stealing other people's idea, so they may be a bit shit.
--- monty_hall_gen.py
+++ monty_hall_gen.py
@@ -2,45 +2,67 @@ import random
 import tqdm
 import argparse
 
+
+def simulation(switch: bool, tests: int, doors: list[int]):
+	i = 0
+	hits = 0
+	pbar = tqdm.tqdm(total=tests)
+	
+	while i < tests:
+		doors_copy = doors.copy()
+		chosen_door = random.choice(doors_copy)
+		correct_door = random.choice(doors_copy)
+		
+		doors_copy.remove(chosen_door)
+		temp = False
+		if correct_door in doors_copy:
+			doors_copy.remove(correct_door)
+			temp = True
+		
+		doors_copy.remove(random.choice(doors_copy))
+		if temp:
+			doors_copy.append(correct_door)
+		
+		if switch:
+			chosen_door = random.choice(doors_copy)
+		
+		if correct_door == chosen_door:
+			hits += 1
+		
+		i += 1
+		pbar.update(1)
+	
+	pbar.close()
+	hits = round(hits / tests, 4)
+	print(f'Percentage of games won: {hits * 100}%')
+
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument('-t', '--tests', help='number of tests', type=int, default=100000000)
-parser.add_argument('-s', '--no_switch', help='flag specifing not to switch doors', action='store_false')
 parser.add_argument('-d', '--doors', help='number of doors', type=int, default=3)
 
 args = parser.parse_args()
 
 tests = args.tests
 doors = [j for j in range(args.doors)]
-switch = args.switch
-i = 0
-hits = 0
-pbar = tqdm.tqdm(total=tests)
 
-while i < tests:
-	doors_copy = doors.copy()
-	chosen_door = random.choice(doors_copy)
-	correct_door = random.choice(doors_copy)
-	
-	doors_copy.remove(chosen_door)
-	temp = False
-	if correct_door in doors_copy:
-		doors_copy.remove(correct_door)
-		temp = True
-	
-	doors_copy.remove(random.choice(doors_copy))
-	if temp:
-		doors_copy.append(correct_door)
-	
-	if switch:
-		chosen_door = random.choice(doors_copy)
-	
-	if correct_door == chosen_door:
-		hits += 1
-	
-	i+=1
-	pbar.update(1)
+print(f'Probability of winning with {len(doors)} doors when switching: ', end='')
+prob = 1/(args.doors - 2)
+prob = 1 + prob
+prob = prob/args.doors
+prob = round(prob, 4) * 100
+print(f'{prob}%\n')
 
-pbar.close()
-hits = round(hits/tests, 4)
-print(f'{hits*100}%')
+print(f'Simulating games with {len(doors)} doors and switching...')
+simulation(switch=True, tests=tests, doors=doors)
+print()
+
+print(f'Probability of winning with {len(doors)} doors but no switching: ', end='')
+prob = 1/args.doors
+prob = round(prob, 4) * 100
+print(f'{prob}%\n')
+
+print(f'Simulating games with {len(doors)} doors but no switching...')
+simulation(switch=False, tests=tests, doors=doors)
+
